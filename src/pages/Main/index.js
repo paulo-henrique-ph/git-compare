65e20124df5877f2f7e8f1abae25c7aa1b15daa0
Paulo Henrique Gomes Pinto <paulo.henrique@kumulus.com.br>
2020-01-23 19:22:57 -0300

    initial commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.png';
+
+import { Container, Form } from './styles';
+
+import CompareList from '../../components/CompareList/index';
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      repositoryError: false,
+      repositoryInput: '',
+      repositories: [],
+    };
+  }
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const { repositoryInput, repositories } = this.state;
+    try {
+      const { data: repository } = await api.get(`/repos/${repositoryInput}`);
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, repository],
+        repositoryError: false,
+      });
+    } catch (err) {
+      this.setState({ repositoryError: true });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  render() {
+    const {
+      repositoryInput, repositories, repositoryError, loading,
+    } = this.state;
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form withError={repositoryError} onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={({ target }) => { this.setState({ repositoryInput: target.value }); }}
+          />
+          <button type="submit">
+            {loading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}
+          </button>
+        </Form>
+
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+display: flex;
+padding-top: 60 px;
+margin-top: 20px;
+flex-direction: column;
+align-items: center;
+`;
+
+export const Form = styled.form`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 55px;
+    padding: 0 20px;
+    background: #FFF;
+    border-radius: 5px;
+    font-size: 18px;
+    color: #444;
+
+    border: ${({ withError }) => (withError ? '2px solid #F00' : 0)};
+  }
+
+  button {
+    width: 80px;
+    height: 55px;
+    padding: 0 20px;
+    margin-left: 10px;
+    background: #63F5B0;
+    color: #FFF;
+    border-radius: 5px;
+    font-size: 20px;
+    font-weight: bold;
+
+    &:hover {
+      background: #52D89F;
+    }
+
+    &:active {
+      background: #02A357;
+    }
+  }
+`;
